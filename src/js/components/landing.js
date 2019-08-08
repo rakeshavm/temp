a5064cfd4307992ae7be70798f342dc012a2ba6d
ak5123 <kanduarul@gmail.com>
2019-08-08 15:31:13 +0530

    3.0

--- src/js/components/landing.js
+++ src/js/components/landing.js
@@ -8,6 +8,74 @@ import './dots.css'
 // import logo from './../../svg/poly.svg';
 class Landing extends React.Component{
 
+    componentDidMount(){
+
+        
+
+            'use strict';
+        
+            function extend( a, b ) {
+                for( var key in b ) { 
+                    if( b.hasOwnProperty( key ) ) {
+                        a[key] = b[key];
+                    }
+                }
+                return a;
+            }
+        
+            function DotNav( el, options ) {
+                this.nav = el;
+                this.options = extend( {}, this.options );
+                extend( this.options, options );
+                this._init();
+            }
+        
+            DotNav.prototype.options = {};
+        
+            DotNav.prototype._init = function() {
+                // special case "dotstyle-hop"
+                var hop = this.nav.parentNode.className.indexOf( 'dotstyle-hop' ) !== -1;
+        
+                var dots = [].slice.call( this.nav.querySelectorAll( 'li' ) ), current = 0, self = this;
+        
+                dots.forEach( function( dot, idx ) {
+                    dot.addEventListener( 'click', function( ev ) {
+                        ev.preventDefault();
+                        if( idx !== current ) {
+                            dots[ current ].className = '';
+        
+                            // special case
+                            if( hop && idx < current ) {
+                                dot.className += ' current-from-right';
+                            }
+        
+                            setTimeout( function() {
+                                dot.className += ' current';
+                                current = idx;
+                                if( typeof self.options.callback === 'function' ) {
+                                    self.options.callback( current );
+                                }
+                            }, 25 );                        
+                        }
+                    } );
+                } );
+            }
+        
+            // add to global namespace
+            window.DotNav = DotNav;
+
+            [].slice.call( document.querySelectorAll( '.dotstyle > ul' ) ).forEach( function( nav ) {
+                new DotNav( nav, {
+                    callback : function( idx ) {
+                    }
+                } );
+            } );
+        
+        
+
+        
+    }
+
     
     
     renderDots(){
